f8dc20813613e081cca9271f8cac8359426069e0
Justine <jkaeslin@edp.ch>
2021-12-18 13:28:18 +0100

    bilder werden jetzt angezeigt einene Artikel erfasst

--- controller/using_ejs.js
+++ controller/using_ejs.js
@@ -37,7 +37,7 @@ app.use(router.allowedMethods());
 // Steht diese Middleware vor den Routes, werden die Routes nicht verarbeitet!
 app.use(async (context) => {
     await send(context, context.request.url.pathname, {
-        root: `${Deno.cwd()}/styles`,
+        root: `${Deno.cwd()}/assets`,
     });
 }); 
 
