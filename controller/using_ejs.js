7aa6cacf2df5b7bd64b35dfaed33390adddd468c
edp-darios <dstuebi@edp.ch>
2022-01-16 20:06:51 +0100

    login seite

--- controller/using_ejs.js
+++ controller/using_ejs.js
@@ -94,7 +94,7 @@ router.get("/product_detail&:id", async (context) => {
 router.get("/kasse_login", async (context) => {
     try {
         context.response.body = await renderFileToString(Deno.cwd() + 
-        "/views/kasse_login.ejs", { product: products, basket: basket });
+            "/views/kasse_login.ejs", { product: products, basket: basket });
         context.response.type = "html";           
     } catch (error) {
         console.log(error);
